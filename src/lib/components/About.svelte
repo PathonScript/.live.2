99132e35313b1837930c8d226627a96075001033
PathonScript <pathonscript@gmail.com>
2022-07-30 11:12:06 +0700

    about me 1.0

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -1,8 +1,24 @@
-<script>
-  import { scrollto } from 'svelte-scrollto';
-</script>
-
-
 <div class="fullflex">
-	<h1 use:scrollto={'#about'} class="text-8xl">About me</h1>
+	<h1 class="flex text-8xl text-white engtle">About me</h1>
+	
+	<div class="grid grid-cols-1 sm:grid-cols-2 h-1/2 mt-10">
+		<div class="grid p-2 mx-4">
+			<h1 class="text-white text-2xl script">
+				Hello, I'm Pat, I'm a student from Thailand and is currently codes for hobby by making fun projects.
+			</h1>
+			<h1 class="text-white text-2xl script">
+				I've been coding since I was about 11. Being inspired by Dani who is a game developer.
+				So I wrote my first line of code in C# for Unity.
+			</h1>
+		</div>
+		<div class="grid p-2 mx-4">
+			<h1 class="text-white text-2xl script">
+				I can speak Thai, English, a bit Chinese. And a tiny Dutch.
+			</h1>
+		</div>
+	</div>
+
+	<!-- <div class="flex flex-row">
+		<div class="flex"></div>
+	</div> -->
 </div>
